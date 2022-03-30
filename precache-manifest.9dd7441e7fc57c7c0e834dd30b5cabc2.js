3724b175d06bf13ce822ea08ed1da4ad9746e675
ADMIN <jpratsv@gmail.com>
2022-03-30 20:19:34 +0200

    Updates

---
+++ precache-manifest.9dd7441e7fc57c7c0e834dd30b5cabc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad73184d3059f75a0259",
+    "url": "/todolist/static/css/main.39837689.chunk.css"
+  },
+  {
+    "revision": "ad73184d3059f75a0259",
+    "url": "/todolist/static/js/main.ad73184d.chunk.js"
+  },
+  {
+    "revision": "1d4a69f649b4fd843ba0",
+    "url": "/todolist/static/js/1.1d4a69f6.chunk.js"
+  },
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "cc253dc63de52314a7fa2629bd0b147c",
+    "url": "/todolist/index.html"
+  }
+];
